f7f32e27add550214ddb6d9a3e00413be5a84861
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-05-29 21:40:23 +0700

    export services

---
+++ src/services/index.js
@@ -0,0 +1,13 @@
+import UserService from './user';
+import AuthService from './auth';
+import CategoryService from './category';
+import GiveawayRegistrationService from './giveaway-registration';
+import GiveawayService from './giveaway';
+
+export default {
+	UserService,
+	AuthService,
+	CategoryService,
+	GiveawayRegistrationService,
+	GiveawayService
+};
