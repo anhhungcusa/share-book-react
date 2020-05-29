716d563655ae26dc5933f26dee8dd70b957c81cf
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-05-29 14:19:54 +0700

    add user service

---
+++ src/services/user.js
@@ -0,0 +1,73 @@
+import { axios } from "../config/axios";
+export const pathname = '/users' 
+
+const registerUser = ({email, password, username}, token) => {
+  return axios({
+    url: pathname,
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    method: "post",
+    data: {
+      email,
+      password,
+      name: username
+    }
+  })
+    .then(res => res.data.message)
+    .catch(err => {
+      const messageError =
+        (err.response && err.response.data && err.response.data.message) ||
+        "registration failed";
+      throw new Error(messageError);
+    });
+};
+
+const fetchUserById = (userId, token) => {
+    return axios({
+      url: `${pathname}/${userId}`,
+      method: 'get',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+    })
+      .then(res => res.data.user)
+      .catch(err => {
+        const messageError =
+          (err.response && err.response.data && err.response.data.message) ||
+          "failed to fetch user";
+        throw new Error(messageError);
+      });
+}
+
+const fetchGiveawaysByUser = (userId, token, {skip, limit, ...query}) => {
+  return axios({
+    url: `${pathname}/${userId}/giveaways`,
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+    method: 'get',
+    params: {
+      skip, limit,
+      ...query
+
+    }
+  })
+    .then(res => res.data.giveaways)
+    .catch(err => {
+      const messageError =
+        (err.response && err.response.data && err.response.data.message) ||
+        "failed to fetch giveaway";
+      throw new Error(messageError);
+    });
+
+}
+
+
+
+export default {
+  registerUser,
+  fetchUserById,
+  fetchGiveawaysByUser
+};
+
