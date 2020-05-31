ea8d6993f0e99858c300b36cb627db6b1323e087
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-05-31 11:29:45 +0700

    setup app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { DataProvider } from './context/DataProvider';
+import { Header } from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { LoginPage } from './pages/Login/Login';
+import { RegisterPage } from './pages/Register/Register';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Share Book'
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <DataProvider>
+          <Header title='Share Book' />
+          <Switch>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <Route path="/register">
+              <RegisterPage />
+            </Route>
+          </Switch>
+        </DataProvider>
+      </Router>
     </div>
   );
 }
