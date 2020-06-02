cc6b2364156802dd114c27d27557a03a6416ec33
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-02 16:18:22 +0700

    update GiveawayCard

--- src/components/GiveawayCard/GiveawayCard.js
+++ src/components/GiveawayCard/GiveawayCard.js
@@ -4,7 +4,7 @@ import moment from 'moment'
 import {UserOutlined} from '@ant-design/icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faExpandArrowsAlt} from '@fortawesome/free-solid-svg-icons'
-import { Card, Avatar, Space } from 'antd'
+import { Card, Avatar, Space, Alert, Tag } from 'antd'
 
 export const GiveawayCard = ({giveaway}) => {
     const {title, gift, description, byUser, begin, end, numParticipants, result} = giveaway
@@ -16,15 +16,16 @@ export const GiveawayCard = ({giveaway}) => {
                 <FontAwesomeIcon icon={faExpandArrowsAlt} />
             ]}
             cover={<img alt="gift-img" src={gift.image} />}
-        >
+        >   
+        <div>
             <Card.Meta avatar={ <Avatar  size='default' icon={<UserOutlined />} />} 
-            title={byUser.username} description={description && description.slice(0, 50) + description.length > 50 ? '...' : ''} />
+            title={byUser.username} description={<span className="giveaway-description">{description}</span>} />
+        </div>
             <Space className="giveaway-info" size={2} direction='vertical'  >
-                {  numParticipants&& <span>number of participants: {numParticipants} </span>}
-                {  result && <span>lucky number: {result.winningNumbers} </span>}
-                <span>start at {moment(begin).format('DD/MM/YYYY ss:mm:HH')}</span>
-                { end && <span>end at{moment(begin).format('DD/MM/YYYY ss:mm:HH')}</span>}
-
+                {  result && <Tag color="#87d068" >lucky number: {result.winningNumbers} </Tag>}
+                {  numParticipants&& <Tag color="#108ee9" >number of participants: {numParticipants} </Tag>}
+                <Tag color="#108ee9">start at {moment(begin).format('DD/MM/YYYY ss:mm:HH')}</Tag>
+                { end && <Tag color="#f50">end at {moment(begin).format('DD/MM/YYYY ss:mm:HH')}</Tag>}
             </Space>
         </Card>
     )
