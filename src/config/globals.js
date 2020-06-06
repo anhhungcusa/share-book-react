ff29db4243fe55e61ff7a253aee213560cffe13d
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-06 12:50:30 +0700

    add domain variable

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,6 +1,7 @@
 export const env = {
     SERVER_URL: process.env.REACT_APP_SERVER_URL || 'http://localhost:8080/api/v1',
-    COOKIE_SECRET_KEY: process.env.REACT_APP_COOKIE_SECRET_KEY || 'SHARE-OLD-BOOK-id'
+    COOKIE_SECRET_KEY: process.env.REACT_APP_COOKIE_SECRET_KEY || 'SHARE-OLD-BOOK-id',
+    DOMAIN: document.domain
 }
 
 //https://share-book-001.herokuapp.com/
